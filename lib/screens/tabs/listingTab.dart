bd16c7ca4b0de98d35b7103b65802c520e45a7da
eisenhauer-37 <eisenhauer.37@buckeyemail.osu.edu>
2020-11-14 18:59:17 -0500

    Merge branch 'master' of C:\Users\nmeis\AndroidStudioProjects\hackohio2020 with conflicts.

--- lib/screens/tabs/listingTab.dart
+++ lib/screens/tabs/listingTab.dart
@@ -6,8 +6,94 @@ class ListingTab extends StatefulWidget {
 }
 
 class _ListingTabState extends State<ListingTab> {
+  final _formKey = GlobalKey<FormState>();
+  List<String> promptText = List<String>();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final _textController = TextEditingController();
+    promptText.add('Create New Listing: ');
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(height: 20.0),
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                  child: Text(
+                    promptText[0],
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 22.0,
+                        fontWeight: FontWeight.bold),
+                  )),
+            ),
+          ),
+          SizedBox(height: 10.0),
+          Flexible(
+            child: Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: TextFormField(
+                controller: _textController,
+                style: TextStyle(color: Colors.black),
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.sort_by_alpha),
+                  hintText: 'Name of Product',
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+            ),
+          ),
+                    Flexible(
+            child: Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: TextFormField(
+                style: TextStyle(color: Colors.black),
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.description),
+                  hintText: 'Description',
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Text(
+            "Upload your image here:",
+//            style: TextStyle(
+//              fontStyle:
+//            ),
+          ),
+          Padding(
+            padding:
+            const EdgeInsets.symmetric(vertical: 16.0, horizontal: 10.0),
+            child: RaisedButton(
+              onPressed: () {
+
+              },
+              child: Text('Submit'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
-}
+  }
+

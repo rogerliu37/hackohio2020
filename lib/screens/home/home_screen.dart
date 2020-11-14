aa2da8efd7b02dbb2904ca7a1deecc1365c43c11
Tyler Liu <42123666+TylerLiu88@users.noreply.github.com>
2020-11-14 15:28:47 -0500

    Merge branch 'master' of /Users/tylerliu/AndroidStudioProjects/hackohio20200 with conflicts.

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -2,43 +2,71 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:shop_app/constants.dart';
 import 'package:shop_app/screens/home/components/body.dart';
+import 'package:shop_app/screens/tabs/homeTab.dart';
+import 'package:shop_app/screens/tabs/listingTab.dart';
+import 'package:shop_app/screens/tabs/profileTab.dart';
+import 'package:shop_app/screens/tabs/searchTab.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+
+  /*List of tabs*/
+  final List<Widget> _children = [
+    new SearchTab(),
+    new HomeTab(),
+    new ListingTab(),
+    new ProfileTab(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: buildAppBar(),
-      body: Body(),
+      body: _children[_currentIndex],
+      //Converted BottomTabBar
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTabTapped,
+        currentIndex: _currentIndex,
+        iconSize: 30.0,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.grey[900],
+        items: [
+          //HomeTab
+          BottomNavigationBarItem(
+            title: new Text(""),
+            icon: new Icon(Icons.home, color: Colors.grey),
+            activeIcon: new Icon(Icons.home, color: Colors.white),
+          ),
+          //SearchTab
+          BottomNavigationBarItem(
+            title: new Text(""),
+            icon: new Icon(Icons.search, color: Colors.grey),
+            activeIcon: new Icon(Icons.search, color: Colors.white),
+          ),
+          //ListingTab
+          BottomNavigationBarItem(
+            title: new Text(""),
+            icon: new Icon(Icons.add_box, color: Colors.grey),
+            activeIcon: new Icon(Icons.add_box, color: Colors.white),
+          ),
+          //profile
+          BottomNavigationBarItem(
+            title: new Text(""),
+            icon: new Icon(Icons.person, color: Colors.grey),
+            activeIcon: new Icon(Icons.person, color: Colors.white),
+          ),
+        ],
+      ),
     );
   }
 
-  AppBar buildAppBar() {
-    return AppBar(
-      backgroundColor: Colors.white,
-      elevation: 0,
-      leading: IconButton(
-        icon: SvgPicture.asset("assets/icons/back.svg"),
-        onPressed: () {},
-      ),
-      actions: <Widget>[
-        IconButton(
-          icon: SvgPicture.asset(
-            "assets/icons/search.svg",
-            // By default our  icon color is white
-            color: kTextColor,
-          ),
-          onPressed: () {},
-        ),
-        IconButton(
-          icon: SvgPicture.asset(
-            "assets/icons/cart.svg",
-            // By default our  icon color is white
-            color: kTextColor,
-          ),
-          onPressed: () {},
-        ),
-        SizedBox(width: kDefaultPaddin / 2)
-      ],
-    );
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
   }
 }
---
+++ lib/screens/tabs/listingTab.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ListingTab extends StatefulWidget {
+  @override
+  _ListingTabState createState() => _ListingTabState();
+}
+
+class _ListingTabState extends State<ListingTab> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
